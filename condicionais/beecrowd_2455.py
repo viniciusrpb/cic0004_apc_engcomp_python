1fb53b48b5abec2b37f7937b2184ec889da44a41
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2023-10-16 10:36:47 -0300

    Update beecrowd_2455.py

--- condicionais/beecrowd_2455.py
+++ condicionais/beecrowd_2455.py
@@ -1,4 +1,13 @@
-p1,c1,p2,c2 = (int(x) for x in input().split(" "))
+p1,c1,p2,c2 = map(int, input().split())
+
+'''
+# Leitura tambem pode ser feita desta maneira:
+p1,c1,p2,c2 = input().split()
+p1 = int(p1)
+c1 = int(c1)
+p2 = int(p2)
+c2 = int(c2)
+'''
 
 if(p1*c1 > p2*c2):
   print("-1")
