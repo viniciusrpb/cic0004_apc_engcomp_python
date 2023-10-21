720ce58e0a0769fb2f136b741bc724a501d00a29
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2023-10-20 21:45:28 -0300

    Add files via upload

---
+++ provas/beecrowd_2058.py
@@ -0,0 +1,10 @@
+'''CIC0004 - Algoritmos e Programação de Computadores
+   Beecrowd 2058 - Triângulos e Polígonos
+   Prof. Dr. Vinicius Ruela Pereira Borges '''
+
+l,n = map(int,input().split())
+
+n = int(input())
+
+print(n-2)
+
